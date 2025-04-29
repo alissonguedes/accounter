7b000f0abde6b6280a5d069b804f0583549595da
Alisson Guedes <alissonguedes87@gmail.com>
2025-04-29 10:33:02 -0300

    first commit

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { CommonModule, formatNumber } from '@angular/common';
 import { TitleDirective } from '../../directives/title/title.directive';
+declare const ApexCharts: any;
 
 @Component({
 	selector: 'app-dashboard',
@@ -10,14 +11,23 @@ import { TitleDirective } from '../../directives/title/title.directive';
 })
 export class DashboardComponent {
 
+	public categorias = [];
+
 	public meses = [
 		'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
 	];
+
+	public mesesAbr = [
+		'jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'
+	];
+
+	public resumoMensal = [];
 
 	public resumo: any = [];
 
 	constructor() {
 		this.getResumoMensal();
+		this.getChart();
 	}
 
 	getResumoMensal(): void {
@@ -42,6 +52,72 @@ export class DashboardComponent {
 
 		this.resumo = [{ 'e': ladoEsquerdo, 'd': ladoDireito }];
 
+	}
+
+	getChart() {
+		setTimeout(() => {
+
+			let data = [];
+			let categories = [];
+
+			let entradas = [];
+			let saidas = [];
+			let investimentos = [];
+			let caixa_emercial = [];
+
+			for (let i = 0; i < this.meses.length; i++) {
+
+				let mes = this.mesesAbr[i].toUpperCase();
+				let num = Math.random() * 100;
+
+				categories.push(mes);
+				data.push(num);
+
+			}
+
+			var options = {
+				chart: {
+					type: 'line',
+					stroke: {
+						curve: 'smooth',
+					}
+				},
+				series: [{
+					type: 'line',
+					name: 'sales',
+					data: data,
+				}, {
+					type: 'column',
+					name: 'sales',
+					data: data,
+				}, {
+					type: 'line',
+					name: 'other',
+					data: data,
+				}],
+				xaxis: {
+					show: false,
+					categories: categories
+				},
+				dataLabels: {
+					enabled: false,
+					formatter: function (value: any, { seriesIndex, dataPointIndex, w }: any) {
+						return value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+					}
+				},
+				yaxis: {
+					show: true,
+					decimalsInFloat: 2,
+					labels: {
+						formatter: function (value: any, { seriesIndex, dataPointIndex, w }: any) {
+							return value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+						}
+					}
+				}
+			}
+			var chart = new ApexCharts(document.querySelector("#chart"), options);
+			chart.render();
+		});
 	}
 
 }
